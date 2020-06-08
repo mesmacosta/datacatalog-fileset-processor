f474acd400999c2b3fe934d18b2ee1a445d94bb9
mesmacosta <mesmacosta@gmail.com>
2020-06-08 16:59:34 -0300

    Bump version: 0.1.3 → 0.1.4

--- src/datacatalog_fileset_processor/__init__.py
+++ src/datacatalog_fileset_processor/__init__.py
@@ -4,6 +4,6 @@ from .datacatalog_fileset_processor_cli import main  # noqa: F401
 
 __author__ = """Marcelo Miranda"""
 __email__ = 'mesmacosta@gmail.com'
-__version__ = '0.1.3'
+__version__ = '0.1.4'
 
 __all__ = ('main')
